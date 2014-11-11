065ff3b3a13b5ae481ae18d717fa24851d9f3763
unknown <p004149A@6GBSL12.Parsons.com>
2014-11-11 10:51:04 -0800

    enable command line for msysgit

--- autorun.cmd
+++ autorun.cmd
@@ -1,3 +1,7 @@
 @echo off
-DOSKEY ls=ls -hF --color=tty $*
+:: bind `subl` to call sublime text
 DOSKEY subl="C:\Program Files\Sublime Text 2\sublime_text.exe" $*
+:: enable Unix commands come with msysgit
+set PATH=%PATH%;"C:\Program Files (x86)\Git\bin"
+:: highlight with `ls` command
+DOSKEY ls=ls -hF --color=tty $*
